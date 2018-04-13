afaa23d883be71a31e567b21f6cd7dafc9648eb5
Otavio Augusto <tavioalves@gmail.com>
2018-04-13 16:54:05 -0300

    Adição de 'dumb' route

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Hello extends React.Component {
+  render () { return <div>Hello!</div> };
+}
+
+class Bye extends React.Component {
+  render () { return <div>Bye!</div> }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/hello" component={Hello} />
+      <Route path="/bye" component={Bye} />
+    </div>
+  </BrowserRouter>
+  ,document.getElementById('root'));
+
+  registerServiceWorker();

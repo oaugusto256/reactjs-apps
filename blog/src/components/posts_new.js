b6e9cc0ae8b811b0e8dbdfdef83d27f3e3e26386
Otavio Augusto <tavioalves@gmail.com>
2018-04-16 15:58:57 -0300

    Adição de validação no form de criação de novo post

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -12,15 +12,22 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     );
   }
+
+  onSubmitPost(values) {
+    console.log(values);
+  }
   
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div>
         <h3>New post</h3>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmitPost.bind(this))}>
           <Field
             label="Title for post"
             name="title"
@@ -36,12 +43,30 @@ class PostsNew extends Component {
             name="content"
             component={this.renderField}
           />
-        </form>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>        
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
